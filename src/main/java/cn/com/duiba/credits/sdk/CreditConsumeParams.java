3459e558a10d033ed7b51dae502a5aae97f07332
xuhengfei <xuhengfei.me@gmail.com>
2014-06-25 11:23:52 +0800

    update

--- src/main/java/cn/com/duiba/credits/sdk/CreditConsumeParams.java
+++ src/main/java/cn/com/duiba/credits/sdk/CreditConsumeParams.java
@@ -12,6 +12,9 @@ public class CreditConsumeParams {
 	private Integer credits;//消耗积分数
 	private String orderNum;//兑吧订单号
 	private String description;
+	private String type="";//类型：QB,Phonebill,AlipayFast,Coupon
+	private Integer facePrice=0;//面值，分为单位
+	private Integer actualPrice=0;//实际扣款，分为单位
 	private String uid;
 	private boolean waitAudit=false;//是否等待审核， 如果返回true，表示此订单需要审核，审核通过后才会继续下去。 如果返回false表示此订单无须审核，会直接继续兑换流程
 	public Integer getCredits() {
@@ -49,6 +52,9 @@ public class CreditConsumeParams {
 		map.put("appSecret", appSecret);
 		map.put("timestamp", timestamp.getTime()+"");
 		map.put("orderNum", orderNum);
+		map.put("type", type);
+		map.put("facePrice", facePrice+"");
+		map.put("actualPrice", actualPrice+"");
 		
 		String sign=SignTool.sign(map);
 		
