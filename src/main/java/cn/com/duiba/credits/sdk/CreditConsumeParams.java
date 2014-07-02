699ed6448c65aa751ec34272c5491d84027a8093
xuhengfei <xuhengfei.me@gmail.com>
2014-07-02 09:37:00 +0800

    update

--- src/main/java/cn/com/duiba/credits/sdk/CreditConsumeParams.java
+++ src/main/java/cn/com/duiba/credits/sdk/CreditConsumeParams.java
@@ -12,12 +12,12 @@ public class CreditConsumeParams {
 	private Integer credits;//消耗积分数
 	private String orderNum;//兑吧订单号
 	private String description;
-	private String type="";//类型：QB,Phonebill,AlipayFast,Coupon
+	private String type="";//类型：QB,Phonebill,AlipayFast,Coupon  所有类型不区分大小写
 	private Integer facePrice=0;//面值，分为单位
 	private Integer actualPrice=0;//实际扣款，分为单位
-	private String alipay="";//支付宝账号
-	private String phone="";//手机号
-	private String qq="";//qq号
+	private String alipay="";//支付宝账号,type为alipayfast时会有值
+	private String phone="";//手机号,type为phonebill时会有值
+	private String qq="";//qq号,type为qb时会有值
 	private String uid;
 	private boolean waitAudit=false;//是否等待审核， 如果返回true，表示此订单需要审核，审核通过后才会继续下去。 如果返回false表示此订单无须审核，会直接继续兑换流程
 	public Integer getCredits() {
